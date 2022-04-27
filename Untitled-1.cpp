44d7655cb2d37b12678ffa21f856d4f6941dcc26
Artemp23 <52859043+Artemp23@users.noreply.github.com>
2022-04-27 22:48:45 +0300

    Create Untitled-1.cpp

---
+++ Untitled-1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+void transcript();
+
+int main()
+
+{
+    transcript();
+    return 0;
+}
+
+void transcript()
+{
+ cout << "Enter word:\n";
+    string word;
+    cin >> word;
+
+    int n = word.length();
+
+    char char_array[255];
+    strcpy(char_array, word.c_str());
+    _strlwr(char_array);
+
+    cout << "You entered: "<< char_array << "\n";
+
+    char char_array2[255];
+    memcpy(char_array2,char_array,255);
+ 
+    for (int i = 0; i < n; i++)
+    {
+        bool b;
+        b = false;
+
+        for (int a = 0; a < n; a++)
+        {   
+           if (char_array[i] == char_array2[a] && i != a)
+           {   
+               b = true;
+               cout << ")";
+               break;
+            }
+           else
+           {
+               if (a == n-1)
+               {cout << "(";}
+           }
+        }
+    }
+}
